4ec7383b821f214ce462d37d86f1d2661fe77c50
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-04-17 18:03:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,28 @@
 <?php
-/**пока оставить так. кажется такая проверка идет ниже
- * Реализовать проверку заполнения обязательных полей формы в предыдущей
- * с использованием Cookies, а также заполнение формы по умолчанию ранее
- * введенными значениями.
- */
-//if (isset($_COOKIE["error_name"]))
-
-
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // Массив для временного хранения сообщений пользователю.
-  $messages = array();
-
-  // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
-  // Выдаем сообщение об успешном сохранении.
+   $messages = array();
   if (!empty($_COOKIE['save'])) {
-    // Удаляем куку, указывая время устаревания в прошлом.
     setcookie('save', '', 100000);
-    // Если есть параметр save, то выводим сообщение пользователю.
     $messages[] = 'Спасибо, результаты сохранены.';
   }
 
-  // Складываем признак ошибок в массив.
   $errors = array();
   $errors['name'] = !empty($_COOKIE['name_error']);$errors['email'] = !empty($_COOKIE['email_error']);
   $errors['date'] = !empty($_COOKIE['date_error']);$errors['gender'] = !empty($_COOKIE['gender_error']);
   $errors['hand'] = !empty($_COOKIE['hand_error']);$errors['biography'] = !empty($_COOKIE['biography_error']);
   $errors['syperpover'] = !empty($_COOKIE['syperpover_error']);
 
-  // Выдаем сообщения об ошибках.
-  foreach ($errors as $er)
- { //if ($errors['name']) {
+   foreach ($errors as $er)
+ { 
   $errorname=key($er) ."_error";
    if ($er) 
-   {
-    // Удаляем куку, указывая время устаревания в прошлом.
-    setcookie($errorname, '', time() - 3600);
-    // Выводим сообщение.
+   { setcookie($errorname, '', time() - 3600);
     if($er=='1') $messages[] = '<div class="error">Заполните поле.</div>';
     else $messages[] = '<div class="error"> Недопустимые символы! </div>';
   }
 }
 
-  // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
   $values['name'] = empty($_COOKIE['name']) ? '' : $_COOKIE['name'];
   $values['email'] = empty($_COOKIE['email']) ? '' : $_COOKIE['email'];
@@ -57,13 +32,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['biography'] = empty($_COOKIE['biography']) ? '' : $_COOKIE['biography'];
   $values['syperpover'] = empty($_COOKIE['syperpover']) ? '' : $_COOKIE['syperpover'];
 
-
-  // Включаем содержимое файла form.php.
-  // В нем будут доступны переменные $messages, $errors и $values для вывода 
-  // сообщений, полей с ранее заполненными данными и признаками ошибок.
-  include('form.php');
+ include('form.php');
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 $formdata=array(
     'name'=>$_POST['name'],
     'email'=>$_POST['email'],
@@ -82,7 +52,6 @@ foreach ($formdata as $v)
   $errorname=key($v) ."_error";
  if (empty($v))
  {
-    // Выдаем куку на день с флажком об ошибке в поле fio.
     setcookie($errorname, '1', time() + 24 * 60 * 60);
     $errors = TRUE;
   }
@@ -92,18 +61,15 @@ foreach ($formdata as $v)
     $errors = TRUE;
   }
   else {
-    // Сохраняем ранее введенное в форму значение на месяц.
     setcookie(key($v), $v, time() + 30 * 24 * 60 * 60);
   }
  }
 
-// При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
   if ($errors) {
     header('Location: index.php');
     exit();
   }
   else {
-    // Удаляем Cookies с признаками ошибок.
     setcookie('name_error', '',time() - 3600);
     setcookie('email_error', '',time() - 3600);
     setcookie('age_error', '',time() - 3600);
@@ -111,8 +77,7 @@ foreach ($formdata as $v)
     setcookie('numberOfLimb_error', '',time() - 3600);
     setcookie('biography_error', '',time() - 3600);
     setcookie('superpower_error', '',time() - 3600);
-    // TODO: тут необходимо удалить остальные Cookies.
-  }
+   }
 
   $name=$_POST['name'];
   $email=$_POST['email'];
@@ -139,9 +104,6 @@ foreach ($formdata as $v)
       exit();
   }
 
-  // Сохраняем куку с признаком успешного сохранения.
   setcookie('save', '1');
-
-  // Делаем перенаправление.
   header('Location: index.php');
 }
