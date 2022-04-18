34ae072acb760072f99969058fe3a05618296c79
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-04-18 18:40:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,65 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-   $messages = array();
-  if (!empty($_COOKIE['save'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') 
+{
+    $messages = array();
+  if (!empty($_COOKIE['save'])) 
+  {
     setcookie('save', '', 100000);
     $messages[] = 'Спасибо, результаты сохранены.';
   }
 
+   $parametrs=array('name', 'email','date','gender','hand','biography','syperpover');
   $errors = array();
-  $errors['name'] = !empty($_COOKIE['name_error']);$errors['email'] = !empty($_COOKIE['email_error']);
-  $errors['date'] = !empty($_COOKIE['date_error']);$errors['gender'] = !empty($_COOKIE['gender_error']);
-  $errors['hand'] = !empty($_COOKIE['hand_error']);$errors['biography'] = !empty($_COOKIE['biography_error']);
-  $errors['syperpover'] = !empty($_COOKIE['syperpover_error']);
-
-   foreach ($errors as $key =>$er)
+  foreach ($parametrs as $it)
+  {
+    $errorname=$it .'_error';
+    if(!empty($_COOKIE[$errorname]))
+    $errors[$it]=empty($_COOKIE[$errorname]);
+  }
+  $formassage=array('name'=>" Имя", 'email'=>" Электронная почта",'date'=>" Дата рождения",'gender'=>" Пол",'hand'=>" Конечности",'biography'=>" Биография",'syperpover'=>" Суперспособность");
+  foreach ($errors as $key =>$er)
  { 
   $errorname=$key ."_error";
-   if ($er) 
-   { setcookie($errorname, '', time() - 3600);
-    if($er=='1') $messages[] = '<div class="error">Заполните поле.</div>';
-    else $messages[] = '<div class="error"> Недопустимые символы! </div>';
+  if ($er) 
+  { setcookie($errorname, '', time() - 3600);
+    if($er=='1') $messages[] = '<div class="error">Заполните поле'.$formassage[$key].'.</div>';
+    else $messages[] = '<div class="error"> Недопустимые символы в поле'.$formassage[$key].'! </div>';
   }
-
+ }
 
   $values = array();
-  $values['name'] = empty($_COOKIE['name']) ? '' : $_COOKIE['name'];
+  foreach ($parametrs as $it)
+  {
+    //if(!empty($_COOKIE[$errorname]))
+    $values[$it]=empty($_COOKIE[$it]);
+  }
+  /* $values['name'] = empty($_COOKIE['name']) ? '' : $_COOKIE['name'];
   $values['email'] = empty($_COOKIE['email']) ? '' : $_COOKIE['email'];
   $values['date'] = empty($_COOKIE['date']) ? '' : $_COOKIE['date'];
   $values['gender'] = empty($_COOKIE['gender']) ? '' : $_COOKIE['gender'];
   $values['hand'] = empty($_COOKIE['hand']) ? '' : $_COOKIE['hand'];
   $values['biography'] = empty($_COOKIE['biography']) ? '' : $_COOKIE['biography'];
   $values['syperpover'] = empty($_COOKIE['syperpover']) ? '' : $_COOKIE['syperpover'];
+  */
 
  include('form.php');
 }
-
-   }
 else {
    $formdata=array(
     'name'=>$_POST['name'],
     'email'=>$_POST['email'],
     'date'=>$_POST['date'],
-    'date'=>$_POST['gender'],
+    'gender'=>$_POST['gender'],
     'hand'=>$_POST['hand'],
     'biography'=>$_POST['biography'],
     'check'=>$_POST['check'],
     'syperpover'=>implode(',',$_POST['syperpover']),
-);
+     );
   // Проверяем ошибки.
   $errors = FALSE;
-foreach ($formdata as  $key =>$v)
-{
+  foreach ($formdata as  $key =>$v)
+ {
   $errorname=$key ."_error";
  if (empty($v))
  {
@@ -74,11 +83,11 @@ foreach ($formdata as  $key =>$v)
   else {
     setcookie('name_error', '',time() - 3600);
     setcookie('email_error', '',time() - 3600);
-    setcookie('age_error', '',time() - 3600);
+    setcookie('date_error', '',time() - 3600);
     setcookie('gender_error', '',time() - 3600);
-    setcookie('numberOfLimb_error', '',time() - 3600);
+    setcookie('hand_error', '',time() - 3600);
     setcookie('biography_error', '',time() - 3600);
-    setcookie('superpower_error', '',time() - 3600);
+    setcookie('syperpover_error', '',time() - 3600);
    }
 
   $name=$_POST['name'];
