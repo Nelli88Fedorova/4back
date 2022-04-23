7497550ab1f8138b546ebf69fcf3143f87f048b3
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-04-23 19:09:56 +0300

    Update form.php

--- form.php
+++ form.php
@@ -75,7 +75,7 @@ input
              </label><br />
 <?php if ($no==false) print('<div style="color:red"> Необходимо согласия на обработку данных!</div>'); ?>
              <br />
-             <label><input type="checkbox"
+             <label><input type="checkbox" value="Yes"
               name="check" />
                Согласен(а) на обработку данных. </label><br />
              
